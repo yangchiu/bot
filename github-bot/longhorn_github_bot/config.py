fd2109e207d86cf27c62cae4cf8e0e734eec77ef
Yang Chiu <yang.chiu@suse.com>
2023-01-10 09:41:26 +0800

    refine org and repo
    Signed-off-by: Yang Chiu <yang.chiu@suse.com>

--- github-bot/longhorn_github_bot/config.py
+++ github-bot/longhorn_github_bot/config.py
@@ -7,9 +7,9 @@ class BotConfig(RequiredConfigMixin):
     required_config.add_option('flask_loglevel', parser=str, default='info', doc='Set the log level for Flask.')
     required_config.add_option('flask_password', parser=str, doc='Password for HTTP authentication in Flask.')
     required_config.add_option('flask_username', parser=str, doc='Username for HTTP authentication in Flask.')
-    required_config.add_option('github_owner', parser=str, default='longhorn', doc='Set the owner of the target GitHub '
+    required_config.add_option('github_owner', parser=str, default='yyz-field', doc='Set the owner of the target GitHub'
                                                                                    'repository.')
-    required_config.add_option('github_repository', parser=str, default='longhorn', doc='Set the name of the target '
+    required_config.add_option('github_repository', parser=str, default='test', doc='Set the name of the target '
                                                                                         'GitHub repository.')
     required_config.add_option('github_token', parser=str, doc='Set the token of the GitHub machine user.')
     required_config.add_option('zenhub_pipeline', parser=str, default='Review', doc='Set the target ZenHub pipeline to '
